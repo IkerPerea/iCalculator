74d4ab4cef5b1a038c601da78e9ba4ef61cb1f63
IkerPerea <ikerpereatrejo@gmail.com>
2024-07-05 01:10:26 +0200

    Initial Commit

---
+++ iCalculator/iCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalculatorApp.swift
+//  iCalculator
+//
+//  Created by Iker Perea on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct iCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
